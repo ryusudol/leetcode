9c87c09a133319a6452f0edcb73e8c3b39aca79b
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-08 22:59:47 +0900

    Time: 101 ms (41.93%) | Memory: 41 MB (8.13%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -7,28 +7,28 @@ class Solution:
         accs = {}
         for i in range(len_of_points - 1):
             for j in range(i + 1, len_of_points):
-                curr_x, curr_y = points[i][0], points[i][1]
-                next_x, next_y = points[j][0], points[j][1]
-                if curr_x == next_x:
-                    same_line = 10000 + curr_x
-                    val = curr_x
+                x1, y1 = points[i][0], points[i][1]
+                x2, y2 = points[j][0], points[j][1]
+                if x1 == x2:
+                    same_line = 10000 + x1
+                    val = x1
                     if (same_line, val) not in accs:
-                        accs[(same_line, val)] = {(curr_x, curr_y), (next_x, next_y)}
+                        accs[(same_line, val)] = {(x1, y1), (x2, y2)}
                     else:
-                        if (curr_x, curr_y) not in accs[(same_line, val)]:
-                            accs[(same_line, val)].add((curr_x, curr_y))
-                        if (next_x, next_y) not in accs[(same_line, val)]:
-                            accs[(same_line, val)].add((next_x, next_y))
+                        if (x1, y1) not in accs[(same_line, val)]:
+                            accs[(same_line, val)].add((x1, y1))
+                        if (x2, y2) not in accs[(same_line, val)]:
+                            accs[(same_line, val)].add((x2, y2))
                     continue
-                acc = (curr_y - next_y) / (curr_x - next_x)
-                val = curr_y - acc * curr_x
+                acc = (y1 - y2) / (x1 - x2)
+                val = y1 - acc * x1
                 if (acc, val) not in accs:
-                    accs[(acc, val)] = {(curr_x, curr_y), (next_x, next_y)}
+                    accs[(acc, val)] = {(x1, y1), (x2, y2)}
                 else:
-                    if (curr_x, curr_y) not in accs[(acc, val)]:
-                        accs[(acc, val)].add((curr_x, curr_y))
-                    if (next_x, next_y) not in accs[(acc, val)]:
-                        accs[(acc, val)].add((next_x, next_y))
+                    if (x1, y1) not in accs[(acc, val)]:
+                        accs[(acc, val)].add((x1, y1))
+                    if (x2, y2) not in accs[(acc, val)]:
+                        accs[(acc, val)].add((x2, y2))
         max_count_values = max(accs, key=lambda k: len(accs[k]))
         max_count = len(accs[max_count_values])
         return max_count
