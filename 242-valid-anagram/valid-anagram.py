af7b51a2cd8b93df0a9b23cfb695cff5bcdf6ee7
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-09 16:20:54 +0900

    Time: 59 ms (25.70%) | Memory: 17.3 MB (54.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        len_s = len(s)
-        len_t = len(t)
-        if len_s != len_t:
-            return False
-        memo = {}
-        for c in s:
-            int_c = ord(c)
-            if int_c not in memo:
-                memo[int_c] = 1
-            else:
-                memo[int_c] += 1
-        for c in t:
-            int_c = ord(c)
-            if int_c not in memo or memo[int_c] == 0:
-                return False
-            else:
-                memo[int_c] -= 1
-        val_set = set(memo.values())
-        if 1 in val_set:
-            return False
-        return True
+        sorted_s = sorted(s)
+        sorted_t = sorted(t)
+        return sorted_s == sorted_t
+        # len_s = len(s)
+        # len_t = len(t)
+        # if len_s != len_t:
+        #     return False
+        # memo = {}
+        # for c in s:
+        #     int_c = ord(c)
+        #     if int_c not in memo:
+        #         memo[int_c] = 1
+        #     else:
+        #         memo[int_c] += 1
+        # for c in t:
+        #     int_c = ord(c)
+        #     if int_c not in memo or memo[int_c] == 0:
+        #         return False
+        #     else:
+        #         memo[int_c] -= 1
+        # val_set = set(memo.values())
+        # if 1 in val_set:
+        #     return False
+        # return True
