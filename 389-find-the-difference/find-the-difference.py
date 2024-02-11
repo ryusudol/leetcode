3752ad4c6dfe0e5d6fe35960363fc7f21141c4f6
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-11 21:03:45 +0900

    Time: 35 ms (81.96%) | Memory: 16.6 MB (62.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,14 +1,9 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        if len(s) == 0:
-            return t
-        memo = {}
-        for c in s:
-            if c not in memo: memo[c] = 1
-            else: memo[c] += 1
-        for c in t:
-            if c not in memo: return c
-            else: memo[c] -= 1
-        for el in memo:
-            if memo[el] == -1:
-                return el
+        if len(s) == 0: return t
+        s = list(s)
+        t = list(t)
+        s.sort()
+        t.sort()
+        for idx, c in enumerate(t):
+            if idx == len(t) - 1 or c != s[idx]: return c
