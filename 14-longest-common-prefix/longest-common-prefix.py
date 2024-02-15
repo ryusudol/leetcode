ac3d2aa52386d90f612debda16ee3ff617f787e3
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-15 16:24:39 +0900

    Time: 47 ms (16.12%) | Memory: 16.6 MB (68.94%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,15 +1,29 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        longest_prefix = strs[0]
-        for _str in strs[1:]:
-            if _str == '':
-                return ''
-            _str = list(_str)
-            for idx, c in enumerate(_str):
-                if idx >= len(longest_prefix):
+        def common_prefix(str1, str2):
+            res = ''
+            for idx, c in enumerate(str1):
+                if idx < len(str2) and c == str2[idx]:
+                    res += c
+                else:
                     break
-                if c != longest_prefix[idx]:
-                    longest_prefix = longest_prefix[:idx]
-                if idx == len(_str) - 1 and idx < len(longest_prefix) - 1:
-                    longest_prefix = longest_prefix[:idx+1]
+            return res
+            # return ''.join([c1 for c1, c2 in zip(str1, str2) if c1 == c2])
+        longest_prefix = strs[0]
+        for s in strs[1:]:
+            longest_prefix = common_prefix(longest_prefix, s)
         return longest_prefix
+
+        # longest_prefix = strs[0]
+        # for _str in strs[1:]:
+        #     if _str == '':
+        #         return ''
+        #     _str = list(_str)
+        #     for idx, c in enumerate(_str):
+        #         if idx >= len(longest_prefix):
+        #             break
+        #         if c != longest_prefix[idx]:
+        #             longest_prefix = longest_prefix[:idx]
+        #         if idx == len(_str) - 1 and idx < len(longest_prefix) - 1:
+        #             longest_prefix = longest_prefix[:idx+1]
+        # return longest_prefix
