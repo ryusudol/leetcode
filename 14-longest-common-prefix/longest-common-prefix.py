e4ee29ff8da42897cf689bb7f4dd9f071000c745
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-15 16:25:29 +0900

    Time: 37 ms (68.57%) | Memory: 16.8 MB (53.49%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -8,22 +8,7 @@ class Solution:
                 else:
                     break
             return res
-            # return ''.join([c1 for c1, c2 in zip(str1, str2) if c1 == c2])
         longest_prefix = strs[0]
         for s in strs[1:]:
             longest_prefix = common_prefix(longest_prefix, s)
         return longest_prefix
-
-        # longest_prefix = strs[0]
-        # for _str in strs[1:]:
-        #     if _str == '':
-        #         return ''
-        #     _str = list(_str)
-        #     for idx, c in enumerate(_str):
-        #         if idx >= len(longest_prefix):
-        #             break
-        #         if c != longest_prefix[idx]:
-        #             longest_prefix = longest_prefix[:idx]
-        #         if idx == len(_str) - 1 and idx < len(longest_prefix) - 1:
-        #             longest_prefix = longest_prefix[:idx+1]
-        # return longest_prefix
