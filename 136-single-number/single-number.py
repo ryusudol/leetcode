80e4d9ca0e80037441e54ca60afd06ddd82136b3
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-16 23:23:08 +0900

    Time: 119 ms (38.35%) | Memory: 19.2 MB (66.04%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,12 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        once = []
-        memo = {}
-        for num in nums:
-            if num not in memo:
-                memo[num] = 1
-                once.append(num)
+        if len(nums) == 1:
+            return nums[0]
+        nums.sort()
+        idx = 0
+        while idx < len(nums):
+            if idx != len(nums) - 1 and nums[idx] == nums[idx+1]:
+                idx += 2
             else:
-                once.remove(num)
-                memo[num] += 1
-        return once[0]
+                return nums[idx]
+
+        # once = []
+        # memo = {}
+        # for num in nums:
+        #     if num not in memo:
+        #         memo[num] = 1
+        #         once.append(num)
+        #     else:
+        #         once.remove(num)
+        #         memo[num] += 1
+        # return once[0]
