b5676c69e84ae41e7a1ee094b466b68cef96698e
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-17 21:59:33 +0900

    Time: 71 ms (40.18%) | Memory: 18.5 MB (81.09%) - LeetSync

--- 598-range-addition-ii/range-addition-ii.py
+++ 598-range-addition-ii/range-addition-ii.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
-        # matrix = [[0] * n for _ in range(m)]
         if not ops:
             return m * n
         min_a = min_b = 999999
@@ -9,10 +8,3 @@ class Solution:
             min_a = min(min_a, a)
             min_b = min(min_b, b)
         return min_a * min_b
-        # count = 0
-        # max_val = max([max(row) for row in matrix])  # Finding the max value in a 2-dimensional array
-        # for i in range(m):
-        #     for j in range(n):
-        #         if matrix[i][j] == max_val:
-        #             count += 1
-        # return count
