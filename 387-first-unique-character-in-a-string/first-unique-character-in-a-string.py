50d50d9a569f7ecc0a3c9912a536fcbb5bc13e02
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2024-02-19 13:47:01 +0900

    Time: 106 ms (31.01%) | Memory: 17.5 MB (10.20%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,15 +1,22 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        arr = [(0, -1)] * 26  # count, first_idx
-        for idx, c in enumerate(s):
-            arr_idx = ord(c) - 97
-            count, first_idx = arr[arr_idx]
-            count = count + 1
-            if count == 1: first_idx = idx
-            arr[arr_idx] = (count, first_idx)
+        memo = {}
         for c in s:
-            arr_idx = ord(c) - 97
-            count, first_idx = arr[arr_idx]
-            if count == 1:
-                return first_idx
+            memo[c] = memo.get(c, 0) + 1
+        for i in range(len(s)):
+            if memo[s[i]] == 1:
+                return i
         return -1
+        # arr = [(0, -1)] * 26  # count, first_idx
+        # for idx, c in enumerate(s):
+        #     arr_idx = ord(c) - 97
+        #     count, first_idx = arr[arr_idx]
+        #     count = count + 1
+        #     if count == 1: first_idx = idx
+        #     arr[arr_idx] = (count, first_idx)
+        # for c in s:
+        #     arr_idx = ord(c) - 97
+        #     count, first_idx = arr[arr_idx]
+        #     if count == 1:
+        #         return first_idx
+        # return -1
