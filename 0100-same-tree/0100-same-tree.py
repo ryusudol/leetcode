19504f14b553507ba74cc75e1f810555aa2a2213
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2025-07-30 16:19:10 +0900

    Time: 0 ms (100%), Space: 17.9 MB (20.91%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if (not p and q) or (p and not q):
+            return False
+        elif not p and not q:
+            return True
+
+        queue_p, queue_q = deque([('c', 0, p)]), deque([('c', 0, q)])
+        while queue_p and queue_q:
+            pos_p, cur_level_p, cur_p = queue_p.popleft()
+            pos_q, cur_level_q, cur_q = queue_q.popleft()
+
+            if pos_p != pos_q or cur_level_p != cur_level_q or cur_p.val != cur_q.val:
+                return False
+
+            if cur_p.left:
+                queue_p.append(('l', cur_level_p + 1, cur_p.left))
+            if cur_q.left:
+                queue_q.append(('l', cur_level_q + 1, cur_q.left))
+            if cur_p.right:
+                queue_p.append(('r', cur_level_p + 1, cur_p.right))
+            if cur_q.right:
+                queue_q.append(('r', cur_level_q + 1, cur_q.right))
+        return queue_p == queue_q
