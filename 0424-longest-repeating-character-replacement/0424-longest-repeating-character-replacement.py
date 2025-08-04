17fabc4b0a4935b083a3283b3c5b6fc079724e1a
Suhyeon Yu <50139277+ryusudol@users.noreply.github.com>
2025-08-04 16:21:20 +0900

    Time: 79 ms (97.7%), Space: 18.1 MB (23.86%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,17 +1,11 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         memo = {}
-        start, end = 0, 0
-        while end < len(s):
-            if s[end] not in memo:
-                memo[s[end]] = 1
-            else:
-                memo[s[end]] += 1
-            cur_counts = memo.values()
-            if sum(cur_counts) - max(cur_counts) > k:
+        start = max_alp_count = 0
+        for end in range(len(s)):
+            memo[s[end]] = memo.get(s[end], 0) + 1
+            max_alp_count = max(max_alp_count, memo[s[end]])
+            if end - start + 1 - max_alp_count > k:
                 memo[s[start]] -= 1
-                if memo[s[start]] == 0:
-                    del memo[s[start]]
                 start += 1
-            end += 1
-        return max(end - start, 1)
+        return len(s) - start
